6dc2dc1140769a1e96edbd8feafdaaf8f42e72ae
Michelangelo Bertoli <108437401+AugustusEst@users.noreply.github.com>
2026-06-24 13:46:31 +0200

    workload e benchmark caliper aggiunti

---
+++ caliper/workloads/createTransaction.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+class CreateTransactionWorkload extends WorkloadModuleBase {
+    async initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext) {
+        await super.initializeWorkloadModule(workerIndex, totalWorkers, roundIndex, roundArguments, sutAdapter, sutContext);
+        this.transactionIndex = 0;
+        this.contractId = this.roundArguments.contractId;
+    }
+
+    async submitTransaction() {
+        const transactionId = `${this.workerIndex}-${this.transactionIndex++}`;
+        const timestamp = new Date().toISOString();
+        const sender = `sender-${this.workerIndex}`;
+        const receiver = `receiver-${this.workerIndex}`;
+        const amount = '100.00';
+        const transactionType = 'PAYMENT';
+        const request = {
+            contractId: this.contractId,
+            contractFunction: 'CreateTransaction',
+            contractArguments: [
+                transactionId,
+                timestamp,
+                sender,
+                receiver,
+                amount,
+                transactionType
+            ],
+            readOnly: false
+        };
+        return this.sutAdapter.sendRequests(request);
+    }
+}
+
+function createWorkloadModule() {
+    return new CreateTransactionWorkload();
+}
+
+module.exports.createWorkloadModule = createWorkloadModule; 
